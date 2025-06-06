fb6db3e46b8c49b61de7433138a4201213f55984
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-06-06 13:31:26 -0400

    postgresql: rename nix file

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.quickdb-postgresql-17 = pkgs.callPackage ./pkgs/postgresql { postgresql = pkgs.postgresql_17; };
+    packages.x86_64-linux.quickdb-postgresql-17 = pkgs.callPackage ./pkgs/postgresql/17.nix { postgresql = pkgs.postgresql_17; };
     packages.x86_64-linux.quickdb-couchdb-3 = pkgs.callPackage ./pkgs/couchdb/3.nix { couchdb = pkgs.couchdb3; };
   };
 }
