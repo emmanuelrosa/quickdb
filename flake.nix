a7fd5484292dca546267eeb37c8927c2b5cf549e
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-09-13 08:31:51 -0400

    add postgresql-18

--- flake.nix
+++ flake.nix
@@ -3,12 +3,16 @@
 
   inputs = {
     nixpkgs1.url = "github:nixos/nixpkgs/?ref=502151620cdde8fda50f1f05706caae833379754";
+    nixpkgs2.url = "github:nixos/nixpkgs/?ref=23da0aa9ec413ed894af3fdc6313e6b8ff623833";
   };
 
-  outputs = { self, nixpkgs1 }: let
+  outputs = { self, nixpkgs1, nixpkgs2 }: let
     pkgs1 = nixpkgs1.legacyPackages.x86_64-linux;
+    pkgs2 = nixpkgs2.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.quickdb-postgresql-17 = pkgs1.callPackage ./pkgs/postgresql/17.nix { postgresql = pkgs1.postgresql_17; };
+    packages.x86_64-linux.quickdb-postgresql-17 = pkgs1.callPackage ./pkgs/postgresql/default.nix { postgresql = pkgs1.postgresql_17; };
+    packages.x86_64-linux.quickdb-postgresql-18 = pkgs2.callPackage ./pkgs/postgresql/default.nix { postgresql = pkgs2.postgresql_18; };
+
     packages.x86_64-linux.quickdb-mariadb-114 = pkgs1.callPackage ./pkgs/mariadb/114.nix { mariadb = pkgs1.mariadb_114; };
     packages.x86_64-linux.quickdb-couchdb-3 = pkgs1.callPackage ./pkgs/couchdb/3.nix { couchdb = pkgs1.couchdb3; };
   };
