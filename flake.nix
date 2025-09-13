ac979e002aa3918225d3efdf1ddfaabfbb7a0e6f
Emmanuel Rosa <emmanuelrosa@protonmail.com>
2025-09-13 08:17:04 -0400

    refactor inputs to be snapshots of nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "Run a database system as a portable application, using nothing but Nix.";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    nixpkgs1.url = "github:nixos/nixpkgs/?ref=502151620cdde8fda50f1f05706caae833379754";
   };
 
-  outputs = { self, nixpkgs }: let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { self, nixpkgs1 }: let
+    pkgs1 = nixpkgs1.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.quickdb-postgresql-17 = pkgs.callPackage ./pkgs/postgresql/17.nix { postgresql = pkgs.postgresql_17; };
-    packages.x86_64-linux.quickdb-mariadb-114 = pkgs.callPackage ./pkgs/mariadb/114.nix { mariadb = pkgs.mariadb_114; };
-    packages.x86_64-linux.quickdb-couchdb-3 = pkgs.callPackage ./pkgs/couchdb/3.nix { couchdb = pkgs.couchdb3; };
+    packages.x86_64-linux.quickdb-postgresql-17 = pkgs1.callPackage ./pkgs/postgresql/17.nix { postgresql = pkgs1.postgresql_17; };
+    packages.x86_64-linux.quickdb-mariadb-114 = pkgs1.callPackage ./pkgs/mariadb/114.nix { mariadb = pkgs1.mariadb_114; };
+    packages.x86_64-linux.quickdb-couchdb-3 = pkgs1.callPackage ./pkgs/couchdb/3.nix { couchdb = pkgs1.couchdb3; };
   };
 }
